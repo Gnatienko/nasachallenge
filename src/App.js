7d81580cd925846ad166f6522b3e1fbca9222878
Gnatienko <gnatienko@gmail.com>
2023-10-07 14:43:04 +0300

    +

--- src/App.js
+++ src/App.js
@@ -42,17 +42,22 @@ function App() {
         <div className="menu-container">
           <Radio
             value="Mercury"
-            top="1.5%"
-            left="14%"
+            top="0.5%"
+            left="11%"
             onChange={handleChange}
           />
           <Radio value="Earth" top="1.5%" left="32%" onChange={handleChange} />
           <Radio
             value="Jupiter"
             top="1.5%"
-            left="50%"
+            left="53%"
             onChange={handleChange}
           />
+          <Radio value="Uranus" top="5%" left="75%" onChange={handleChange} />
+          <Radio value="Venus" top="40%" left="21%" onChange={handleChange} />
+          <Radio value="Mars" top="43%" left="41%" onChange={handleChange} />
+          <Radio value="Saturn" top="40%" left="63%" onChange={handleChange} />
+          <Radio value="Neptune" top="47%" left="85%" onChange={handleChange} />
         </div>
         <img className="solar-system-img" src={solar_system} alt="img" />
         <div className="text">
